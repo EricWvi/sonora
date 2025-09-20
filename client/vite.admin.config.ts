60b6f8e540afbac1890cb482188d51b2aee76183
Eric Wang <eric88wang@qq.com>
2025-09-20 15:13:40 +0000

    init client

---
+++ client/vite.admin.config.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  publicDir: "public-journal",
+  build: {
+    outDir: "journal",
+    rollupOptions: {
+      input: path.resolve(__dirname, "admin.html"),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5273,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8765",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
